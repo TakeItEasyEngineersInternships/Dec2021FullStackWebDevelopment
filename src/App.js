2c4c0f5cfd32ee83170ec77359f272a76cd7b685
Surabhi0729 <85481125+Surabhi0729@users.noreply.github.com>
2022-01-05 19:24:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
-import "./App.css";
-import Home from "./pages/Home.js";
-import Sem from "./pages/Sem";
-import FirstSemPhy from "./pages/sems/FirstSemPhy.js";
-import FirstSemchem from "./pages/sems/FirstSemchem";
-import ThirdSem from "./pages/sems/ThirdSem";
-import FourthSem from "./pages/sems/FourthSem";
-import FifthSem from "./pages/sems/FifthSem";
-import SixthSem from "./pages/sems/SixthSem";
-import Branch7 from "./pages/sems/7thSem/Branch7";
-import CSE from "./pages/sems/7thSem/CSE";
-import EighthSem from "./pages/sems/EighthSem";
-import EC from "./pages/sems/7thSem/EC";
-import CGPACalc from "./pages/CGPACalc";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react'
+import NavBar from './components/NavBar'
+import './App.css'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './screens/home'
+import Cgpa from './screens/cgpa' 
+import Sgpa from './screens/sgpa'
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/sem" component={Sem} />
-          <Route path="/fistsemphy" component={FirstSemPhy} />
-          <Route path="/fistsemchem" component={FirstSemchem} />
-          <Route path="/thirdsem" component={ThirdSem} />
-          <Route path="/fourthsem" component={FourthSem} />
-          <Route path="/fifthsem" component={FifthSem} />
-          <Route path="/sixthsem" component={SixthSem} />
-          <Route path="/branch7" component={Branch7} />
-          <Route path="/cse7" component={CSE} />
-          <Route path="/ec7" component={EC} />
-          <Route path="/eightsem" component={EighthSem} />
-          <Route path="/cgpacalc" component={CGPACalc} />
-        </Switch>
-      </div>
-    </Router>
-  );
+    return ( 
+        <div>
+            <BrowserRouter>
+            <NavBar />
+            <Route exact path = '/' > <Home /> </Route> 
+            <Route path = '/cgpa' > <Cgpa /> </Route> 
+            <Route path = '/sgpa'> <Sgpa /> </Route> 
+            </ BrowserRouter >
+        </div>
+    )
 }
-
 export default App;
