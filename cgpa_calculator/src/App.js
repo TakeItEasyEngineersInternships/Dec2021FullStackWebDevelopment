3ffa3c6c3f37094578668bfdbc23cecbcc56883e
alok <you@example.com>
2022-01-06 16:51:05 +0530

    CGPA Calculator Fix Issue Updated react-router-dom to version 6

--- cgpa_calculator/src/App.js
+++ cgpa_calculator/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Home from "./pages/Home.js";
+import Sem from "./pages/Sem";
+import FirstSemPhy from "./pages/sems/FirstSemPhy.js";
+import FirstSemchem from "./pages/sems/FirstSemchem";
+import ThirdSem from "./pages/sems/ThirdSem";
+import FourthSem from "./pages/sems/FourthSem";
+import FifthSem from "./pages/sems/FifthSem";
+import SixthSem from "./pages/sems/SixthSem";
+import Branch7 from "./pages/sems/7thSem/Branch7";
+import CSE from "./pages/sems/7thSem/CSE";
+import EighthSem from "./pages/sems/EighthSem";
+import EC from "./pages/sems/7thSem/EC";
+import CGPACalc from "./pages/CGPACalc";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/sem" element={<Sem />} />
+          <Route path="/fistsemphy" element={<FirstSemPhy />} />
+          <Route path="/fistsemchem" element={<FirstSemchem />} />
+          <Route path="/thirdsem" element={<ThirdSem />} />
+          <Route path="/fourthsem" element={<FourthSem />} />
+          <Route path="/fifthsem" element={<FifthSem />} />
+          <Route path="/sixthsem" element={<SixthSem/>} />
+          <Route path="/branch7" element={<Branch7/>} />
+          <Route path="/cse7" element={<CSE/>} />
+          <Route path="/ec7" element={<EC/>} />
+          <Route path="/eightsem" element={<EighthSem/>} />
+          <Route path="/cgpacalc" element={<CGPACalc/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
